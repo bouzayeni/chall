1ddac190a94305c48ce0e513861de1799683f38e
khalil <khalil94bouzayeni^gmail.com>
2021-11-19 12:46:43 +0100

    challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [task,setTask]=useState("")
+  const[listTask,setListTask] =  useState([])
+  const handleAdd = (e) => {
+    e.preventDefault()
+    setListTask([...listTask, {id:uuidv4(),desc:task,isDone:false}])
+    setTask('')
+  }
+
+ const handleDelete = (id) => {
+   const newList = listTask.filter((item)=>item.id !== id)
+   setListTask(newList)
+ }
+ 
+ const handleDone = (id) => {
+  setListTask(listTask.map((e)=> 
+  e.id == id ? {...e,isDone:!e.isDone}: e
+  ) )
+ }
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="set your task" name="task" value={task} onChange={(e)=>setTask(e.target.value)}/> 
+      <button onClick={handleAdd}>Add</button>
+      <div>
+          {listTask.map((elt) => (
+            <>
+              <p style={{
+  textDecoration: elt.isDone ? 'line-through' : 'none'
+}}>{elt.desc}</p>
+              <button onClick={()=>{handleDelete(elt.id)}}>Delete</button> 
+              <button onClick={()=>{handleDone(elt.id)}}>Done</button>
+            </>
+          ))}
+          
+       
+        
+      </div>
     </div>
   );
 }
